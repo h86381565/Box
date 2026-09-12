a2519bd1982e1dbde8299411c03a17a9d1e81361
RadiantMeteor✨ <151913031+h86381565@users.noreply.github.com>
2026-09-12 23:44:53 +0800

    Update PlayerHelper.java

--- app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
@@ -1,6 +1,8 @@
 package com.github.tvbox.osc.util;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 
 import com.blankj.utilcode.util.ToastUtils;
 import com.github.tvbox.osc.api.ApiConfig;
@@ -28,55 +30,29 @@ public class PlayerHelper {
     }
 
     public static void updateCfg(VideoView videoView, JSONObject playerCfg, int forcePlayerType) {
-        int playerType = Hawk.get(HawkConfig.PLAY_TYPE, 0);
+        int playerType = Hawk.get(HawkConfig.PLAY_TYPE, 2);
         int renderType = Hawk.get(HawkConfig.PLAY_RENDER, 0);
-        String ijkCode = Hawk.get(HawkConfig.IJK_CODEC, "软解码");
+        String ijkCode = Hawk.get(HawkConfig.IJK_CODEC, "硬解码");
         int scale = Hawk.get(HawkConfig.PLAY_SCALE, 0);
         try {
-            playerType = playerCfg.getInt("pl");
-            //就我遇到的问题是 Exo 在 TextureView 黑屏 调整设置中的渲染模式无法生效
-            //renderType = playerCfg.getInt("pr");//该值无法修改，一旦确认该值后续无法进行修改 就是在设置选的 类型无法应用
-            ijkCode = playerCfg.getString("ijk");
-            scale = playerCfg.getInt("sc");
+            if (playerCfg != null) {
+                if (playerCfg.has("pl")) playerType = playerCfg.getInt("pl");
+                if (playerCfg.has("ijk")) ijkCode = playerCfg.getString("ijk");
+                if (playerCfg.has("sc")) scale = playerCfg.getInt("sc");
+                if (playerCfg.has("pr")) renderType = playerCfg.getInt("pr");
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
         if (forcePlayerType >= 0) playerType = forcePlayerType;
+        
+        Hawk.put(HawkConfig.PLAY_TYPE, playerType);
+        Hawk.put(HawkConfig.IJK_CODEC, ijkCode);
+        Hawk.put(HawkConfig.PLAY_RENDER, renderType);
+
         IJKCode codec = ApiConfig.get().getIJKCodec(ijkCode);
-        PlayerFactory playerFactory;
-        if (playerType == 1) {
-            playerFactory = new PlayerFactory<IjkmPlayer>() {
-                @Override
-                public IjkmPlayer createPlayer(Context context) {
-                    return new IjkmPlayer(context, codec);
-                }
-            };
-        } else if (playerType == 2) {
-            playerFactory = new PlayerFactory<EXOmPlayer>() {
-                @Override
-                public EXOmPlayer createPlayer(Context context) {
-                    return new EXOmPlayer(context);
-                }
-            };
-        } else if (playerType == 3) {
-            playerFactory = AliyunMediaPlayerFactory.create();
-        } else {
-            playerFactory = AndroidMediaPlayerFactory.create();
-        }
-        RenderViewFactory renderViewFactory = null;
-        if (playerType==2){
-            renderViewFactory = PlayerViewRenderViewFactory.create(renderType);
-        }else{
-            switch (renderType) {
-                case 0:
-                default:
-                    renderViewFactory = TextureRenderViewFactory.create();
-                    break;
-                case 1:
-                    renderViewFactory = SurfaceRenderViewFactory.create();
-                    break;
-            }
-        }
+        PlayerFactory playerFactory = createPlayerFactory(playerType, codec);
+        RenderViewFactory renderViewFactory = createRenderFactory(playerType, renderType);
 
         videoView.setPlayerFactory(playerFactory);
         videoView.setRenderViewFactory(renderViewFactory);
@@ -84,41 +60,88 @@ public class PlayerHelper {
     }
 
     public static void updateCfg(VideoView videoView) {
-        int playType = Hawk.get(HawkConfig.PLAY_TYPE, 0);
-        PlayerFactory playerFactory;
-        if (playType == 1) {
-            playerFactory = new PlayerFactory<IjkmPlayer>() {
-                @Override
-                public IjkmPlayer createPlayer(Context context) {
-                    return new IjkmPlayer(context, null);
-                }
-            };
-
-        } else if (playType == 2) {
-            playerFactory = new PlayerFactory<EXOmPlayer>() {
-                @Override
-                public EXOmPlayer createPlayer(Context context) {
-                    return new EXOmPlayer(context);
-                }
-            };
-        } else if (playType == 3) {
-            playerFactory = AliyunMediaPlayerFactory.create();
-        } else {
-            playerFactory = AndroidMediaPlayerFactory.create();
-        }
+        int playType = Hawk.get(HawkConfig.PLAY_TYPE, 2);
         int renderType = Hawk.get(HawkConfig.PLAY_RENDER, 0);
-        RenderViewFactory renderViewFactory = null;
-        switch (renderType) {
-            case 0:
-            default:
-                renderViewFactory = TextureRenderViewFactory.create();
-                break;
-            case 1:
-                renderViewFactory = SurfaceRenderViewFactory.create();
-                break;
-        }
+        String ijkCode = Hawk.get(HawkConfig.IJK_CODEC, "硬解码");
+        IJKCode codec = ApiConfig.get().getIJKCodec(ijkCode);
+
+        PlayerFactory playerFactory = createPlayerFactory(playType, codec);
+        RenderViewFactory renderViewFactory = createRenderFactory(playType, renderType);
+        
         videoView.setPlayerFactory(playerFactory);
         videoView.setRenderViewFactory(renderViewFactory);
+    }
+
+    private static PlayerFactory createPlayerFactory(int playerType, IJKCode codec) {
+        PlayerFactory playerFactory;
+        switch (playerType) {
+            case 1:
+                playerFactory = new PlayerFactory<IjkmPlayer>() {
+                    @Override
+                    public IjkmPlayer createPlayer(Context context) {
+                        return new IjkmPlayer(context, codec);
+                    }
+                };
+                break;
+            case 2:
+                playerFactory = new PlayerFactory<EXOmPlayer>() {
+                    @Override
+                    public EXOmPlayer createPlayer(Context context) {
+                        return new EXOmPlayer(context);
+                    }
+                };
+                break;
+            case 3:
+                playerFactory = AliyunMediaPlayerFactory.create();
+                break;
+            case 10: // MX
+                playerFactory = new PlayerFactory<IjkmPlayer>() {
+                    @Override
+                    public IjkmPlayer createPlayer(Context context) {
+                        return new IjkmPlayer(context, codec != null ? codec : ApiConfig.get().getIJKCodec("硬解码"));
+                    }
+                };
+                break;
+            case 11: // Reex
+                playerFactory = new PlayerFactory<EXOmPlayer>() {
+                    @Override
+                    public EXOmPlayer createPlayer(Context context) {
+                        return new EXOmPlayer(context);
+                    }
+                };
+                break;
+            case 12: // Kodi
+                playerFactory = new PlayerFactory<IjkmPlayer>() {
+                    @Override
+                    public IjkmPlayer createPlayer(Context context) {
+                        return new IjkmPlayer(context, ApiConfig.get().getIJKCodec("软解码"));
+                    }
+                };
+                break;
+            case 0:
+            default:
+                playerFactory = AndroidMediaPlayerFactory.create();
+                break;
+        }
+        return playerFactory;
+    }
+
+    private static RenderViewFactory createRenderFactory(int playerType, int renderType) {
+        RenderViewFactory renderViewFactory;
+        if (playerType == 2 || playerType == 11) {
+            renderViewFactory = PlayerViewRenderViewFactory.create(renderType);
+        } else {
+            switch (renderType) {
+                case 1:
+                    renderViewFactory = SurfaceRenderViewFactory.create();
+                    break;
+                case 0:
+                default:
+                    renderViewFactory = TextureRenderViewFactory.create();
+                    break;
+            }
+        }
+        return renderViewFactory;
     }
 
     public static void init() {
@@ -141,6 +164,14 @@ public class PlayerHelper {
         } else {
             return "系统";
         }
+    }
+
+    public static String[] getAllPlayerNames() {
+        return new String[]{"系统", "IJK", "Exo", "阿里", "MX", "Reex", "Kodi"};
+    }
+
+    public static int[] getAllPlayerTypes() {
+        return new int[]{0, 1, 2, 3, 10, 11, 12};
     }
 
     public static String getRenderName(int renderType) {
@@ -183,5 +214,4 @@ public class PlayerHelper {
         }
         return th.getLocalizedMessage();
     }
-
 }
